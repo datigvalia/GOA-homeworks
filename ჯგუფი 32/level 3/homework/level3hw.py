630cdfd7656b3a03d264c3ff1b97cc4ed36feb2d
datigvalia <167451449+datigvalia@users.noreply.github.com>
2024-05-30 20:51:21 +0400

    Add files via upload

---
+++ ჯგუფი 32/level 3/homework/level3hw.py
@@ -0,0 +1,35 @@
+# step 1 
+
+name="dati"
+last_name="gvalia"
+age=14
+hobby="programming"
+country="georgia"
+
+print(name, last_name, age, hobby, country)
+
+#step 2- math
+
+print(14+92)
+
+print(23-12)
+
+print(5*9)
+
+print(50-35)
+
+print(10//5)
+
+print(40+83)
+
+print(40//10)
+
+print(8*4)
+
+print(15//5)
+
+print(90+190)
+ 
+
+ #step 3 ვისწავლე სიტყვები.
+
